38c2e0ee5d608e3dbdd1412d95f5d1d4ff1440fa
Devang <devngc@gmail.com>
2021-11-23 14:01:02 +0530

    feat(*): add vtkjs sub-package

---
+++ ladybug_vtk/vtkjs/helper.py
@@ -0,0 +1,257 @@
+"""This module helps in embedding vtkjs data in Paraview Glance HTML.
+
+This module is originally developed by Kitware and can be found at the following link.
+https://github.com/Kitware/VTK/blob/master/Web/Python/vtkmodules/web/vtkjs_helper.py
+
+"""
+
+import base64
+import json
+import re
+import os
+import shutil
+import sys
+import zipfile
+import pathlib
+
+try:
+    import zlib
+    compression = zipfile.ZIP_DEFLATED
+except ModuleNotFoundError:
+    compression = zipfile.ZIP_STORED
+
+
+def convert_directory_to_zip_file(
+    directory_path: str, remove: bool = True, extension='zip', move=True
+        ) -> str:
+
+    if os.path.isfile(directory_path):
+        return
+
+    zip_file_path = f'{directory_path}.{extension}'
+    zf = zipfile.ZipFile(zip_file_path, mode='w')
+
+    try:
+        for dir_name, _, file_list in os.walk(directory_path):
+            for fname in file_list:
+                full_path = pathlib.Path(dir_name, fname)
+                rel_path = full_path.relative_to(directory_path)
+                zf.write(
+                    full_path.as_posix(),
+                    arcname=rel_path.as_posix(),
+                    compress_type=compression
+                )
+    finally:
+        zf.close()
+
+    if remove:
+        shutil.rmtree(directory_path)
+    if move:
+        return shutil.move(zip_file_path, directory_path)
+    else:
+        return zip_file_path
+
+
+def add_data_to_viewer(data_path, src_html_path=None):
+    template = pathlib.Path(
+        pathlib.Path(__file__).parent, '../assets/PollinationViewer.html'
+    ).resolve().as_posix()
+    src_html_path = src_html_path or template
+    if not os.path.isfile(data_path):
+        raise FileNotFoundError(f'Failed to find {data_path}')
+    if not os.path.exists(src_html_path):
+        raise FileNotFoundError(f'Failed to find source html file: {src_html_path}')
+
+    dstDir = os.path.dirname(data_path)
+    dstHtmlPath = os.path.join(dstDir, "%s.html" % os.path.basename(data_path)[:-6])
+
+    # Extract data as base64
+    with open(data_path, 'rb') as data:
+        data_content = data.read()
+        base64Content = base64.b64encode(data_content)
+        base64Content = base64Content.decode().replace('\n', '')
+
+    # Create new output file
+    with open(src_html_path, mode="r", encoding="utf-8") as srcHtml:
+        with open(dstHtmlPath, mode="w", encoding="utf-8") as dstHtml:
+            for line in srcHtml:
+                if "<!–– insert ––>" in line:
+                    dstHtml.write("<script>\n")
+                    dstHtml.write('var contentToLoad = "%s";\n\n' % base64Content)
+                    dstHtml.write("function _getContent() { return contentToLoad }</script>\n")
+                    continue
+                dstHtml.write(line)
+    return dstHtmlPath
+
+
+def zipAllTimeSteps(directory_path):
+    if os.path.isfile(directory_path):
+        return
+
+    class UrlCounterDict(dict):
+        Counter = 0
+
+        def GetUrlName(self, name):
+            if name not in self.keys():
+                self[name] = str(objNameToUrls.Counter)
+                self.Counter = self.Counter + 1
+            return self[name]
+
+    def InitIndex(sourcePath, destObj):
+        with open(sourcePath, "r") as sourceFile:
+            sourceData = sourceFile.read()
+            sourceObj = json.loads(sourceData)
+            for key in sourceObj:
+                destObj[key] = sourceObj[key]
+            # remove vtkHttpDataSetReader information
+            for obj in destObj["scene"]:
+                obj.pop(obj["type"])
+                obj.pop("type")
+
+    def getUrlToNameDictionary(indexObj):
+        urls = {}
+        for obj in indexObj["scene"]:
+            urls[obj[obj["type"]]["url"]] = obj["name"]
+        return urls
+
+    def addDirectoryToZip(
+        dir_name, zipobj, storedData, rootIdx, timeStep, objNameToUrls
+    ):
+        # Update root index.json file from index.json of this timestep
+        with open(os.path.join(dir_name, "index.json"), "r") as currentIdxFile:
+            currentIdx = json.loads(currentIdxFile.read())
+            urlToName = getUrlToNameDictionary(currentIdx)
+            rootTimeStepSection = rootIdx["animation"]["timeSteps"][timeStep]
+            for key in currentIdx:
+                if key == "scene" or key == "version":
+                    continue
+                rootTimeStepSection[key] = currentIdx[key]
+            for obj in currentIdx["scene"]:
+                objName = obj["name"]
+                rootTimeStepSection[objName] = {}
+                rootTimeStepSection[objName]["actor"] = obj["actor"]
+                rootTimeStepSection[objName]["actorRotation"] = obj["actorRotation"]
+                rootTimeStepSection[objName]["mapper"] = obj["mapper"]
+                rootTimeStepSection[objName]["property"] = obj["property"]
+
+        # For every object in the current timestep
+        for folder in sorted(os.listdir(dir_name)):
+            currentItem = os.path.join(dir_name, folder)
+            if os.path.isdir(currentItem) is False:
+                continue
+            # Write all data array of the current timestep in the archive
+            for filename in os.listdir(os.path.join(currentItem, "data")):
+                full_path = os.path.join(currentItem, "data", filename)
+                if os.path.isfile(full_path) and filename not in storedData:
+                    storedData.add(filename)
+                    rel_path = os.path.join("data", filename)
+                    zipobj.write(full_path, arcname=rel_path, compress_type=compression)
+            # Write the index.json containing pointers to these data arrays
+            # while replacing every basepath as '../../data'
+            objIndexFilePath = os.path.join(dir_name, folder, "index.json")
+            with open(objIndexFilePath, "r") as objIndexFile:
+                objIndexObjData = json.loads(objIndexFile.read())
+            for elm in objIndexObjData.keys():
+                try:
+                    if "ref" in objIndexObjData[elm].keys():
+                        objIndexObjData[elm]["ref"]["basepath"] = "../../data"
+                    if "arrays" in objIndexObjData[elm].keys():
+                        for array in objIndexObjData[elm]["arrays"]:
+                            array["data"]["ref"]["basepath"] = "../../data"
+                except AttributeError:
+                    continue
+            currentObjName = urlToName[folder]
+            objIndexrel_path = os.path.join(
+                objNameToUrls.GetUrlName(currentObjName), str(timeStep), "index.json"
+            )
+            zipobj.writestr(
+                objIndexrel_path,
+                json.dumps(objIndexObjData, indent=2),
+                compress_type=compression,
+            )
+
+    zip_file_path = "%s.zip" % directory_path
+    currentDirectory = os.path.abspath(os.path.join(directory_path, os.pardir))
+    rootIndexPath = os.path.join(currentDirectory, "index.json")
+    rootIndexFile = open(rootIndexPath, "r")
+    rootIndexObj = json.loads(rootIndexFile.read())
+
+    zf = zipfile.ZipFile(zip_file_path, mode="w")
+    try:
+        # We copy the scene from an index of a specific timestep to the root index
+        # Scenes should all have the same objects so only do it for the first one
+        isSceneInitialized = False
+        # currentlyAddedData set stores hashes of every data we already added to the
+        # vtkjs archive to prevent data duplication
+        currentlyAddedData = set()
+        # Regex that folders storing timestep data from paraview should follow
+        reg = re.compile(r"^" + os.path.basename(directory_path) + r"\.[0-9]+$")
+        # We assume an object will not be deleted from a timestep to another so we
+        # create a generic index.json for each object
+        genericIndexObj = {}
+        genericIndexObj["series"] = []
+        timeStep = 0
+        for item in rootIndexObj["animation"]["timeSteps"]:
+            genericIndexObj["series"].append({})
+            genericIndexObj["series"][timeStep]["url"] = str(timeStep)
+            genericIndexObj["series"][timeStep]["timeStep"] = float(item["time"])
+            timeStep = timeStep + 1
+        # Keep track of the url for every object
+        objNameToUrls = UrlCounterDict()
+
+        timeStep = 0
+        # zip all timestep directories
+        for folder in sorted(os.listdir(currentDirectory)):
+            full_path = os.path.join(currentDirectory, folder)
+            if os.path.isdir(full_path) and reg.match(folder):
+                if not isSceneInitialized:
+                    InitIndex(os.path.join(full_path, "index.json"), rootIndexObj)
+                    isSceneInitialized = True
+                addDirectoryToZip(
+                    full_path,
+                    zf,
+                    currentlyAddedData,
+                    rootIndexObj,
+                    timeStep,
+                    objNameToUrls,
+                )
+                shutil.rmtree(full_path)
+                timeStep = timeStep + 1
+
+        # Write every index.json holding time information for each object
+        for name in objNameToUrls:
+            zf.writestr(
+                os.path.join(objNameToUrls[name], "index.json"),
+                json.dumps(genericIndexObj, indent=2),
+                compress_type=compression,
+            )
+
+        # Update root index.json urls and write it in the archive
+        for obj in rootIndexObj["scene"]:
+            obj["id"] = obj["name"]
+            obj["type"] = "vtkHttpDataSetSeriesReader"
+            obj["vtkHttpDataSetSeriesReader"] = {}
+            obj["vtkHttpDataSetSeriesReader"]["url"] = objNameToUrls[obj["name"]]
+        zf.writestr(
+            "index.json", json.dumps(rootIndexObj, indent=2), compress_type=compression
+        )
+        os.remove(rootIndexPath)
+
+    finally:
+        zf.close()
+
+    shutil.move(zip_file_path, directory_path)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(
+            'Usage: python helper.py /path/to/directory.vtkjs '
+            '[/path/to/ParaViewGlance.html]'
+        )
+    else:
+        fileName = sys.argv[1]
+        convert_directory_to_zip_file(fileName)
+
+        if len(sys.argv) == 3:
+            add_data_to_viewer(fileName, sys.argv[2])
---
+++ ladybug_vtk/vtkjs/schema.py
@@ -0,0 +1,132 @@
+"""Schema for VTKJS objects."""
+import json
+import pathlib
+from typing import Dict, List, Tuple
+import enum
+
+from pydantic import BaseModel, Field, validator
+
+
+class DisplayMode(enum.Enum):
+    """Display mode."""
+    Shaded = 2
+    Surface = 2
+    SurfaceWithEdges = 3
+    Wireframe = 1
+    Points = 0
+
+
+class SensorGridOptions(enum.Enum):
+    """Settings for loading sensor grids."""
+    Ignore = 0  # no loading
+    Sensors = 1  # load them as sensor points
+    Mesh = 2  # load them as mesh
+
+
+class Camera(BaseModel):
+    """Camera in vtkjs viewer."""
+    focalPoint: List[float] = Field(
+        [2.5, 5, 1.5],
+        description='Camera focal point.', max_items=3, min_items=3
+    )
+    position: List[float] = Field(
+        [19.3843, -6.75305, 10.2683],
+        description='Camera position.', max_items=3, min_items=3
+    )
+    viewUp: List[float] = Field(
+        [-0.303079, 0.250543, 0.919441],
+        description='View up vector.', max_items=3, min_items=3
+    )
+
+
+class DataSetResource(BaseModel):
+    """Path to a data resource."""
+    url: str = Field(
+        ..., description='Relative path to data resource.'
+    )
+
+
+class DataSetActor(BaseModel):
+    """A Dataset actor."""
+    origin: List[float] = Field([0, 0, 0])
+    scale: List[float] = Field([1, 1, 1])
+    position: List[float] = Field([0, 0, 0])
+
+
+class DataSetMapper(BaseModel):
+    colorByArrayName: str = Field('')
+    colorMode: int = Field(0)
+    scalarMode: int = Field(4)
+
+
+class DataSetProperty(BaseModel):
+    representation: int = Field(2)
+    edgeVisibility: int = Field(0)
+    diffuseColor: List[float] = Field(
+        [0.8, 0.8, 0.8],
+        description='Surface color.', max_items=3, min_items=3
+    )
+    pointSize: int = Field(5)
+    opacity: float = Field(1)
+
+
+class DataSet(BaseModel):
+    """A VTKJS dataset."""
+    name: str = Field(..., description='DataSet name.')
+    type: str = Field('httpDataSetReader')  # I don't know enough about this field!
+    httpDataSetReader: DataSetResource = Field(...)
+    actor: DataSetActor = Field(DataSetActor())
+    actorRotation: List[float] = Field(
+        [0, 0, 0, 1],
+        description='Actor rotation axis.', max_items=4, min_items=4
+    )
+    mapper: DataSetMapper = Field(DataSetMapper())
+    property: DataSetProperty = Field(DataSetProperty())
+    legends: List[dict] = Field(
+        [],
+        description='A list of dictionaries representing legend information for the'
+        ' data added to the Dataset object.'
+    )
+
+
+class IndexJSON(BaseModel):
+    """VTKJS index class.
+
+    These information will be translated to an index.json file.
+    """
+    version = 1
+    background: List[float] = Field(
+        [1, 1, 1],
+        description='Background color.', max_items=3, min_items=3
+    )
+    camera: Camera = Field(
+        Camera(),
+        description='Initial camera in the viewer.'
+    )
+    centerOfRotation: List[float] = Field(
+        [2.5, 5, 1.5],
+        description='X, Y, Z for center of rotation.', max_items=3, min_items=3
+    )
+    scene: List[DataSet] = Field(
+        None, description='List of data set in viewer.'
+    )
+    lookupTables: Dict = Field(
+        None
+    )
+
+    @validator('scene', always=True)
+    def empty_list(cls, v):
+        return [] if v is None else v
+
+    @validator('lookupTables', always=True)
+    def empty_dict(cls, v):
+        return {} if v is None else v
+
+    def to_json(self, folder: str) -> str:
+        """Write the settings as index.json."""
+        data = self.dict()
+        target_folder = pathlib.Path(folder)
+        target_folder.mkdir(parents=True, exist_ok=True)
+        index_file = pathlib.Path(target_folder, 'index.json')
+        index_file.write_text(json.dumps(data))
+        return index_file.as_posix()
