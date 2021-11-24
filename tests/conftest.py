529689c768fe0c860b2a32e330293c87eede3251
Devang <devngc@gmail.com>
2021-11-24 10:21:14 +0530

    test(*): use fixtures in unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from tempfile import TemporaryDirectory
 from pathlib import Path
+from ladybug.epw import EPW
 
 
 @pytest.fixture(scope='session')
@@ -8,3 +9,8 @@ def temp_folder():
     d = TemporaryDirectory()
     yield Path(d.name)
     d.cleanup()
+
+
+@pytest.fixture()
+def epw():
+    return EPW('./tests/assets/weather/boston.epw')
--- tests/sunpath_test.py
+++ tests/sunpath_test.py
@@ -1,19 +1,11 @@
-import pathlib
-
-from ladybug.epw import EPW
 from ladybug.sunpath import Sunpath
 
 
-def test_sunpath():
-    epw = EPW('./tests/assets/weather/boston.epw')
+def test_sunpath(temp_folder, epw):
     location = epw.location
 
-    temp_folder = pathlib.Path('./tests/temp')
-    temp_folder.mkdir(parents=True, exist_ok=True)
-    # Initiate sunpath
     sp = Sunpath.from_location(location)
-    path = sp.to_vtkjs(
-        temp_folder,
-        data = [epw.global_horizontal_radiation, epw.dry_bulb_temperature]
-    )
+    path = sp.to_vtkjs(temp_folder,
+                       data=[epw.global_horizontal_radiation, epw.dry_bulb_temperature]
+                       )
     assert path.name == 'sunpath.vtkjs'
