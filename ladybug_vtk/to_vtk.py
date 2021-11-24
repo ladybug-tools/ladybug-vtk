2ec3917cefc7c0abf42333b3d4f2db751d0b9fda
Devang <devngc@gmail.com>
2021-11-24 11:15:05 +0530

    fix(to_vtk): Add a new function to create Polydata from points

--- ladybug_vtk/to_vtk.py
+++ ladybug_vtk/to_vtk.py
@@ -1,13 +1,31 @@
 """Functions to create VTK objects from Ladybug Objects."""
 
 import vtk
-from typing import List
+from typing import List, Union
 from ladybug_geometry.geometry3d import Point3D, Polyline3D
 from .polydata import PolyData
 
 
-def create_points(points: List[Point3D]) -> PolyData:
-    """Create Polydata from a Polyline."""
+def to_vtk_points(points: List[Point3D]) -> PolyData:
+    """Create Polydata from a list of Points."""
+    vtk_points = vtk.vtkPoints()
+    vtk_vertices = vtk.vtkCellArray()
+
+    for point in points:
+        vtk_points.InsertNextPoint(tuple(point))
+
+    vtk_vertices.InsertNextCell(len(points), list(range(len(points))))
+
+    polydata = PolyData()
+    polydata.SetPoints(vtk_points)
+    polydata.SetVerts(vtk_vertices)
+    polydata.Modified()
+
+    return polydata
+
+
+def to_vtk_polyline_from_points(points: List[Point3D]) -> PolyData:
+    """Create Polydata with a Polyline from a list of points."""
     # Create a vtkPoints container and store the points for all the lines
     pts = vtk.vtkPoints()
     for pt in points:
@@ -35,6 +53,7 @@ def create_points(points: List[Point3D]) -> PolyData:
     return polydata
 
 
-def create_polyline(polyline: Polyline3D) -> PolyData:
+def to_vtk_polyline(polyline: Polyline3D) -> PolyData:
     """Create Polydata from a Polyline."""
-    return create_points(polyline.vertices)
+    points = polyline.vertices
+    return to_vtk_polyline_from_points(points)
