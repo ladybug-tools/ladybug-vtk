1d05dc020d0894372f206c246192c63a1c2ce5e8
Devang <devngc@gmail.com>
2021-11-24 11:15:41 +0530

    style(sunpath_test): minor styling

--- tests/sunpath_test.py
+++ tests/sunpath_test.py
@@ -3,9 +3,9 @@ from ladybug.sunpath import Sunpath
 
 def test_sunpath(temp_folder, epw):
     location = epw.location
-
     sp = Sunpath.from_location(location)
     path = sp.to_vtkjs(temp_folder,
-                       data=[epw.global_horizontal_radiation, epw.dry_bulb_temperature]
+                       data=[epw.global_horizontal_radiation,
+                             epw.dry_bulb_temperature]
                        )
     assert path.name == 'sunpath.vtkjs'
