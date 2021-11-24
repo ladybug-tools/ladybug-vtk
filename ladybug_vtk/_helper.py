5f5e1ab07a9efac3f6be3de5bbf5b0f6f4d521cc
Devang <devngc@gmail.com>
2021-11-24 11:13:56 +0530

    chore(_helper): remove unnecessary functions

--- ladybug_vtk/_helper.py
+++ ladybug_vtk/_helper.py
@@ -1,10 +1,10 @@
 """Helper functions to be used in other modules."""
-from itertools import chain
+
 from typing import Any, Union, List, Tuple
 
 
-def _validate_input(val: Union[List, Tuple], val_type: List[Any], max_val: int = None,
-                    num_val: int = None) -> bool:
+def validate_input(val: Union[List, Tuple], val_type: List[Any], max_val: int = None,
+                   num_val: int = None) -> bool:
     """Validate if all values in the provided input are selected object type and
     less than a specific maximum value.
 
@@ -26,30 +26,3 @@ def _validate_input(val: Union[List, Tuple], val_type: List[Any], max_val: int =
         return all(list(map(lambda v: isinstance(v, tuple(val_type)) and v < max_val, val)))
     else:
         return all(list(map(lambda v: isinstance(v, tuple(val_type)), val)))
-
-
-def get_min_max(val: List[List]) -> Tuple[float, float]:
-    """Get minimum and maximum values from a list of lists.
-
-    Args:
-        val: A list of lists
-
-    Returns:
-        A tuple of minimum and maximum values.
-    """
-    result = list(chain(*val))
-    return (min(result), max(result))
-
-
-def get_line_count(file_path: str) -> int:
-    """Get the number of non empty lines in a file.
-
-    Args:
-        file_path: File path.
-
-    Returns:
-        The number of non empty lines in the file.
-    """
-    with open(file_path) as fp:
-        nonempty_line_count = len([line.strip("\n") for line in fp if line != "\n"])
-    return nonempty_line_count
