6d461ec28af21cd8ea1c6ecb098a88932ab1b3fd
Devang <devngc@gmail.com>
2021-11-30 19:52:53 +0530

    refactor(_extend_ladybug): refactor module based on update functions

--- ladybug_vtk/_extend_ladybug.py
+++ ladybug_vtk/_extend_ladybug.py
@@ -4,10 +4,12 @@ import math
 from pathlib import Path
 
 from typing import List, Union, Tuple, Optional
+from ladybug_geometry.geometry3d import Plane, Arc3D
 from ladybug.sunpath import Sunpath, Point3D, Vector3D
 from ladybug.datacollection import HourlyContinuousCollection
 
-from .to_vtk import to_vtk_points, to_vtk_polyline, to_vtk_polyline_from_points
+from .from_ladybug import from_points, from_polyline, from_arc
+from .to_vtk import to_circle
 from .model_dataset import ModelDataSet
 from .model import Model
 
@@ -31,23 +33,17 @@ def sunpath_to_vtkjs(self, output_folder: str, name: str = 'sunpath', radius: in
 
     polylines = self.hourly_analemma_polyline3d(
         origin=origin, daytime_only=True, radius=radius)
-    sp_polydata = [to_vtk_polyline(pl) for pl in polylines]
+    sp_polydata = [from_polyline(pl) for pl in polylines]
     sp_dataset = ModelDataSet(name='Hourly_Analemmas', data=sp_polydata)
 
     # monthly arcs
     arcs = self.monthly_day_arc3d()
-    arc_polylines = [arc.to_polyline(divisions=10)for arc in arcs]
-    arc_polydata = [to_vtk_polyline(arc) for arc in arc_polylines]
+    arc_polydata = [from_arc(arc, 100) for arc in arcs]
     arc_dataset = ModelDataSet(name='Monthly_Arcs', data=arc_polydata)
 
-    # add a circle
-    north = origin.move(Vector3D(0, radius, 0))
-    plot_points = [
-        north.rotate_xy(math.radians(angle), origin)
-        for angle in range(0, 365, 5)
-    ]
-    plot = to_vtk_polyline_from_points(plot_points)
-    plot_dataset = ModelDataSet(name='Base_Circle', data=[plot])
+    # base circle
+    base_polydata = to_circle(origin, radius)
+    base_dataset = ModelDataSet(name='Base_Circle', data=[base_polydata])
 
     # add suns
     day = self.hourly_analemma_suns(daytime_only=True)
@@ -59,9 +55,9 @@ def sunpath_to_vtkjs(self, output_folder: str, name: str = 'sunpath', radius: in
             pts.append(origin.move(sun.sun_vector.reverse() * radius))
             hours.append(sun.hoy)
 
-    sun_positions = to_vtk_points(pts)
+    sun_positions = from_points(pts)
 
-    datasets = [sp_dataset, arc_dataset, plot_dataset]
+    datasets = [sp_dataset, arc_dataset, base_dataset]
     if data:
         for dt in data:
             assert isinstance(dt, HourlyContinuousCollection), 'Data needs to be a'\
