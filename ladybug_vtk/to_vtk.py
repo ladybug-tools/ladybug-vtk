198066fd46a4cccde67228e916779cad1639b8a9
Devang <devngc@gmail.com>
2021-12-02 18:41:01 +0530

    refactor(to_vtk): updated to_text function

--- ladybug_vtk/to_vtk.py
+++ ladybug_vtk/to_vtk.py
@@ -2,7 +2,9 @@
 
 
 import vtk
+from typing import Union
 from ladybug_geometry.geometry3d import Point3D
+from ladybug_geometry.geometry2d import Point2D
 from .polydata import PolyData
 
 
@@ -29,12 +31,14 @@ def to_circle(center: Point3D, radius: int = 100, sides: int = 100) -> PolyData:
     return polydata
 
 
-def to_text(text: str, point: Point3D) -> PolyData:
+def to_text(text: str, point: Union[Point3D, Point2D], scale: float = 2) -> PolyData:
     """Create a VTK text object from a text string and a ladybug Point3D.
 
     Args:
         text: A text string.
-        point: A ladybug Point3D object. This is the location in 3D space of the text.
+        point: A ladybug Point3D or Point2D object. This is the location in 3D 
+            space of the text.
+        scale: The scale of the text. Defaults to 2.
 
     Returns:
         A Polydata object containing the text.
@@ -43,7 +47,11 @@ def to_text(text: str, point: Point3D) -> PolyData:
     source.SetText(text)
 
     translation = vtk.vtkTransform()
-    translation.Translate(point.x, point.y, point.z)
+    if isinstance(point, Point3D):
+        translation.Translate(point.x, point.y, point.z)
+    else:
+        translation.Translate(point.x, point.y, 0)
+    translation.Scale(scale, scale, scale)
 
     transformFilter = vtk.vtkTransformPolyDataFilter()
     transformFilter.SetInputConnection(source.GetOutputPort())
