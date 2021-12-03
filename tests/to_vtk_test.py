23a74e430fe00db7b4e4f81b9575359e655d0cc6
Devang <devngc@gmail.com>
2021-12-03 13:40:23 +0530

    test(to_vtk_test): add a unit test for to_text

--- tests/to_vtk_test.py
+++ tests/to_vtk_test.py
@@ -1,5 +1,5 @@
 from ladybug_geometry.geometry3d import Point3D
-from ladybug_vtk.to_vtk import to_circle
+from ladybug_vtk.to_vtk import to_circle, to_text
 
 
 def test_to_vtk_circle():
@@ -8,3 +8,11 @@ def test_to_vtk_circle():
     assert polydata.GetNumberOfPoints() == 10
     assert polydata.GetNumberOfCells() == 1
     assert polydata.GetNumberOfLines() == 1
+
+
+def test_to_text():
+    """Test to_text function."""
+    text_polydata = to_text('Hello World!', Point3D(5, 5, 5))
+    assert round(text_polydata.GetBounds()[0], 2) == 5.54
+    assert round(text_polydata.GetBounds()[2], 2) == 4.82
+    assert round(text_polydata.GetBounds()[4], 2) == 5.0
