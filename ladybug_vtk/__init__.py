7c6ba8a87fca041115896150d3871b41a9207c50
Devang <devngc@gmail.com>
2021-12-11 11:15:04 +0530

    style(__init__): extend ladybug geometry first

--- ladybug_vtk/__init__.py
+++ ladybug_vtk/__init__.py
@@ -1,6 +1,6 @@
 """ladybug-vtk library."""
 
 # load all the functions that extends ladybug core library
+import ladybug_vtk._extend_ladybug_geometry
 import ladybug_vtk._extend_sunpath
 import ladybug_vtk._extend_hourly_plot
-import ladybug_vtk._extend_ladybug_geometry
