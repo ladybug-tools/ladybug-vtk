0dcc754cfefa1eaf0eca502bebdb22127f4adc58
mostaphaRoudsari <sadeghipour@gmail.com>
2022-09-21 17:36:38 -0400

    feat: add support for ladybug-display-schema
    I'm not sure if I should have used ladybug-display library instead

--- ladybug_vtk/__init__.py
+++ ladybug_vtk/__init__.py
@@ -2,5 +2,6 @@
 
 # load all the functions that extends ladybug core library
 import ladybug_vtk._extend_ladybug_geometry
+import ladybug_vtk._extend_ladybug_display_schema
 import ladybug_vtk._extend_sunpath
 import ladybug_vtk._extend_hourly_plot
