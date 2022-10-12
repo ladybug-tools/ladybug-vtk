ecda535ce0885626354841860b68ab0dfb95a70b
mostaphaRoudsari <sadeghipour@gmail.com>
2022-10-12 19:19:59 -0400

    feat: extend ladybug-display visualization set with to_vtkjs method
    This means from now on, every object that create  a visualization set can be instantly visualized in pollination viewer.

---
+++ ladybug_vtk/_extend_visualization_set.py
@@ -0,0 +1,34 @@
+"""Extend visualization set object in ladybug display to have a to_vtkjs method.
+
+This extension makes it easier for developers to get started with using this feature
+for developing apps.
+"""
+from pathlib import Path
+from ladybug_display.visualization import VisualizationSet as LBVisualizationSet
+from .visualization_set import VisualizationSet as VTKVisualizationSet, DisplayPolyData
+
+
+def vs_to_vtkjs(
+        self: LBVisualizationSet, output_folder: str,
+        file_name: str = 'visualization_set'
+    ):
+    """
+    Export Visualization set to a vtkjs file.
+
+    Args:
+        output_folder: Path to the target folder to write the vtkjs file.
+        file_name: Output file name. Defaults to visualization_set.
+
+    Returns:
+        A pathlib Path object to the vtkjs file.
+    """
+    data_sets = [
+            DisplayPolyData.from_visualization_geometry(geometry)
+            for geometry in self.geometry
+        ]
+    vs = VTKVisualizationSet(datasets=data_sets)
+    path = vs.to_vtkjs(folder=output_folder, name=file_name)
+    return Path(path)
+
+
+LBVisualizationSet.to_vtkjs = vs_to_vtkjs
--- tests/visualization_test.py
+++ tests/visualization_test.py
@@ -1,14 +1,16 @@
-from ladybug_display.visualization import VisualizationSet
-from ladybug_vtk.visualization_set import VTKVisualizationSet
+from ladybug_vtk.visualization_set import VisualizationSet
 import pathlib
-import json
 
 
 def test_vs(temp_folder, visualization_set):
-    data = json.loads(pathlib.Path(visualization_set).read_text())
-    vs = VisualizationSet.from_dict(data)
-    model = VTKVisualizationSet.from_visualization_set(vs)
-    path = model.to_vtkjs(folder=temp_folder, name='vs-model')
+    vs = VisualizationSet.from_visualization_set(visualization_set)
+    path = vs.to_vtkjs(folder=temp_folder, name='vs-model')
     path = pathlib.Path(path)
     assert path.is_file()
     assert path.name == 'vs-model.vtkjs'
+
+
+def test_extension(temp_folder, visualization_set):
+    path = visualization_set.to_vtkjs(output_folder=temp_folder, file_name='vs-model-2')
+    assert path.is_file()
+    assert path.name == 'vs-model-2.vtkjs'
