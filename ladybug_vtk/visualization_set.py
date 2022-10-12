d9732e886c58db8208f4c441e143e9946f88ce66
mostaphaRoudsari <sadeghipour@gmail.com>
2022-10-12 19:18:32 -0400

    style(VisualizationSet): Rename VTKVisualizationSet to VisualizationSet
    Keep the names simple and based on the namespace.

--- ladybug_vtk/visualization_set.py
+++ ladybug_vtk/visualization_set.py
@@ -7,22 +7,22 @@ import os
 
 
 from typing import List, Union
-from ladybug_display.visualization import VisualizationSet
+from ladybug_display.visualization import VisualizationSet as LBVisualizationSet
 
 from .vtkjs.schema import IndexJSON
 from .vtkjs.helper import convert_directory_to_zip_file
 from .display_polydata import DisplayPolyData
 
 
-class VTKVisualizationSet:
+class VisualizationSet:
 
     def __init__(self, datasets: Union[DisplayPolyData, List[DisplayPolyData]] = None) -> None:
         self.datasets = self._validate_datasets(datasets)
 
     @classmethod
     def from_visualization_set(
-            cls, visualization_set: VisualizationSet) -> VTKVisualizationSet:
-        """Create a VTKVisualizationSet from a Ladybug Display VisualizationSet."""
+            cls, visualization_set: LBVisualizationSet) -> VisualizationSet:
+        """Create a VisualizationSet from a Ladybug Display VisualizationSet."""
         # translate analysis geometry
         data_sets = [
             DisplayPolyData.from_visualization_geometry(geometry)
