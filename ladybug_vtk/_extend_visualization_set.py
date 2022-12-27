d0af59a53f0fc09954a057c704380dc5e2f615bf
mostaphaRoudsari <sadeghipour@gmail.com>
2022-12-27 15:36:50 -0500

    fix(ladybug visualization set): add to_html method to LBVisualizationSet

--- ladybug_vtk/_extend_visualization_set.py
+++ ladybug_vtk/_extend_visualization_set.py
@@ -3,6 +3,7 @@
 This extension makes it easier for developers to get started with using this feature
 for developing apps.
 """
+
 from pathlib import Path
 from ladybug_display.visualization import VisualizationSet as LBVisualizationSet
 from .visualization_set import VisualizationSet as VTKVisualizationSet, DisplayPolyData
@@ -31,4 +32,30 @@ def vs_to_vtkjs(
     return Path(path)
 
 
+def vs_to_html(
+    self: LBVisualizationSet, output_folder: str,
+    file_name: str = 'visualization_set',
+    open: bool = False
+):
+    """
+    Export Visualization set to a HTML file.
+
+    Args:
+        output_folder: Path to the target folder to write the HTML file.
+        file_name: Output file name. Defaults to visualization_set.
+        open: A boolean to open the HTML file once created. Default is set to False.
+
+    Returns:
+        A pathlib Path object to the HTML file.
+    """
+    data_sets = [
+        DisplayPolyData.from_visualization_geometry(geometry)
+        for geometry in self.geometry
+    ]
+    vs = VTKVisualizationSet(datasets=data_sets)
+    path = vs.to_html(folder=output_folder, name=file_name, open=open)
+    return Path(path)
+
+
 LBVisualizationSet.to_vtkjs = vs_to_vtkjs
+LBVisualizationSet.to_html = vs_to_html
