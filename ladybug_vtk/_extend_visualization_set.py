083fafc586a5163e9303d64107437d6ae1ea1c8b
Chris Mackey <chris@ladybug.tools>
2022-12-27 15:36:40 -0500

    fix(extend): Extend VisualizationSet with to_html

--- ladybug_vtk/_extend_visualization_set.py
+++ ladybug_vtk/_extend_visualization_set.py
@@ -31,4 +31,28 @@ def vs_to_vtkjs(
     return Path(path)
 
 
+def vs_to_html(
+    self: LBVisualizationSet, output_folder: str,
+    file_name: str = 'visualization_set'
+):
+    """
+    Export Visualization set to a HTML file with an embedded vtkjs file.
+
+    Args:
+        output_folder: Path to the target folder to write the HTML file.
+        file_name: Output file name. Defaults to visualization_set.
+
+    Returns:
+        A pathlib Path object to the HTML file.
+    """
+    data_sets = [
+        DisplayPolyData.from_visualization_geometry(geometry)
+        for geometry in self.geometry
+    ]
+    vs = VTKVisualizationSet(datasets=data_sets)
+    path = vs.to_html(folder=output_folder, name=file_name)
+    return Path(path)
+
+
 LBVisualizationSet.to_vtkjs = vs_to_vtkjs
+LBVisualizationSet.to_html = vs_to_html
